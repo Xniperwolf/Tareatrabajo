6a62e6677366006cbf5a46ed89902b08fa746551
Ignacio Gutierrez <ig.gutierrezh@duocuc.cl>
2024-07-02 18:27:41 -0400

    Ejercicio realizado

---
+++ Ejerciciotienda.py
@@ -0,0 +1,48 @@
+import os
+import csv
+import time
+os.system("cls")
+
+from funcionestienda import *
+
+ventas = []
+
+while True:
+    print("Bienvenido a la aplicación de tienda Wolf")
+    print("------------------------------------------")
+    print("1. Registrar nueva venta")
+    print("2. Reporte de ventas histórico")
+    print("3. Reporte de ventas por producto")
+    print("4. Reports por forma de pago")
+    print("5. Salir del programa")
+
+    while True:
+        try:
+            opc = int(input("Ingrese una opción: "))
+            if opc in (1,2,3,4,5):
+                break
+            else:
+                print("Ingrese una opción correcta")
+        except:
+            print("Ingrese un número entero")
+    
+    os.system("cls")
+    if opc == 1:
+        registrar_venta()
+
+
+    elif opc == 2:
+        reporte()
+
+                
+
+    elif opc == 3:
+            registroproducto()
+
+    elif opc == 4:
+        registroformadepago()
+
+    else:
+        print("Muchas gracias por ocupar la aplicación")
+        break
+
---
+++ funcionestienda.py
@@ -0,0 +1,70 @@
+ventas = []
+import os
+import csv
+import time
+
+def registrar_venta():
+        os.system("cls")
+        print("Registrar nueva venta")
+        time.sleep(0.5)
+        print("----------------------")
+        time.sleep(0.5)
+        nombreproducto = input("Ingrese nombre del producto: ")
+        cantidadproducto = int(input("Ingrese cantidad de producto: "))
+        valorunitarioproducto = int(input("Ingrese valor unitario del producto: "))
+        formadepagoproducto = int(input("Ingrese forma de pago: (1 para Efectivo, 2 para debito, 3 para crédito, 4 para transferencia)"))
+
+        preciototalventa = valorunitarioproducto*cantidadproducto
+        print("El precio total de la venta es: ",preciototalventa)
+        
+        ventaregistrada = {"Producto": nombreproducto, "Cantidad": cantidadproducto, "Valor Unitario": valorunitarioproducto, "Pago": formadepagoproducto, "Total": preciototalventa}
+        ventas.append(ventaregistrada)
+
+        print("Venta registrada con éxito")
+        time.sleep(2)
+        os.system("cls")
+
+def reporte():
+        os.system("cls")
+        if len(ventas) == 0:
+            print("No hay productos registrados")
+        else:
+            print("Lista de Ventas Históricas")
+            for x in ventas:
+                print(f"Producto: {x['Producto']}\n Cantidad:{x['Cantidad']}\n Valor Unitario:{x['Valor Unitario']}\n Pago:{x['Pago']}\n Total:{x['Total']}")
+                time.sleep(2)
+                os.system("cls")
+
+def registroproducto():
+            nombreproducto = input("Ingrese nombre del producto a buscar: ")
+            for x in ventas:
+                if x['Producto'].lower() == nombreproducto.lower():
+                    print(f"Producto: {x['Producto']}\nCantidad: {x['Cantidad']}\nValor Unitario: {x['Valor Unitario']}\nPago: {x['Pago']}\nTotal: {x['Total']}\n")
+                    break
+                else:
+                    print("Producto no existe")
+
+        
+            with open("Archivo_ventas.csv","w", newline="") as archivo:
+                secciones = ["Producto", "Cantidad", "Valor Unitario", "Pago", "Total"]
+                escritor = csv.DictWriter(archivo, fieldnames=secciones)
+                escritor.writeheader()
+                escritor.writerows(ventas)
+
+def registroformadepago():
+        formadepagoproducto = int(input("Ingrese nombre del producto a buscar (1 para Efectivo, 2 para debito, 3 para crédito, 4 para transferencia): "))
+        for x in ventas:
+            if x['Pago'] == formadepagoproducto:
+                print(f"Producto: {x['Producto']}\nCantidad: {x['Cantidad']}\nValor Unitario: {x['Valor Unitario']}\nPago: {x['Pago']}\nTotal: {x['Total']}\n")
+                break
+            else:
+                print("No se ha pagado con ese medio de pago")
+                
+
+        
+            with open("Archivo_mediosdepago.csv","w", newline="") as archivo:
+                secciones = ["Producto", "Cantidad", "Valor Unitario", "Pago", "Total"]
+                escritor = csv.DictWriter(archivo, fieldnames=secciones)
+                escritor.writeheader()
+                escritor.writerows(ventas)
+
